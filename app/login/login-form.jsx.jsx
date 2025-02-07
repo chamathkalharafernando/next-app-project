4163f809803d5f261939fbdecb68beddcacd9c54
Chamath Fernando <chamadula555@gmail.com>
2025-02-07 19:15:00 +0530

    Adding Logging Form

---
+++ app/login/login-form.jsx.jsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useState } from "react";
+
+// client component
+
+export default function LoginForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Data : ", {email:email,password:password});
+    };
+
+  return (
+    <div className="w-[380px] mx-auto bg-white shadow-md border border-gray-200 rounded-lg p-4">
+        <form onSubmit={handleSubmit} className="space-y-6">
+          {/* Title */}
+          <h3 className="text-center text-xl font-semibold text-gray-900">
+            Sign in Seotech
+          </h3>
+
+          {/* Email */}
+          <div>
+            <label
+              htmlFor="email"
+              className="text-sm font-semibold text-gray-900 block mb-2"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-gray-50 border border-gray-300 rounded-lg text-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="yourname@email.com"
+              required
+            />
+          </div>
+
+          {/* Password */}
+          <div>
+            <label
+              htmlFor="password"
+              className="text-sm font-semibold text-gray-900 block mb-2"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-gray-50 border border-gray-300 rounded-lg text-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="*********"
+              required
+            />
+          </div>
+
+          {/* Remember Me */}
+          <div className="flex justify-between">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  className="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded"
+                />
+              </div>
+              <div className="text-sm ml-3">
+                <label htmlFor="remember" className="font-medium text-gray-900">
+                  Remember me
+                </label>
+              </div>
+            </div>
+
+            <a
+              href="/forget-password"
+              className="text-sm text-blue-700 font-semibold hover:underline"
+            >
+              Forget Password?
+            </a>
+          </div>
+
+          {/* Submit Button*/}
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+          >
+            Sign in
+          </button>
+
+          <div className="flex justify-center text-sm font-medium text-gray-500 space-x-1">
+            <span>Not registered?</span>
+            <a href="/register" className="text-blue-700 hover:underline">
+              Create an account
+            </a>
+          </div>
+        </form>
+      </div>
+  )
+}
