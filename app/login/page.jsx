8a0c9992d9cce3a362cec3a82d8444eadf99f548
Chamath Fernando <chamadula555@gmail.com>
2025-02-11 11:43:45 +0530

    Update the login form

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,7 +4,7 @@ import LoginForm from "./login-form.jsx";
 export default function LoginPage() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen text-black">
-      <LoginForm/> 
+      <LoginForm /> 
     </div>
   );
 }
